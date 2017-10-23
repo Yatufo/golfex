6223a94fbe4b02481854426c5eed62181604312e
Alex Marquez <a-almarquez@expedia.com>
2017-10-23 17:12:52 -0400

    Changing rest client

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var config = require('./config.js')
 var express = require('express')
 var _ = require('lodash');
 var app = express()
-var Client = require('node-rest-client').Client;
-
-var client = new Client();
+var client = require('unirest');
 var ELASTIC_SEARCH_SERVER = process.env.ELASTIC_SEARCH_SERVER
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,26 +13,34 @@ app.get('/', function(req, res) {
 
 app.get('/destinations', function(req, res) {
 
-  var args = {
-    headers: config.defaultHeaders,
-    data: config.destinationEsQuery
-  }
-
   // direct way
-  client.post(ELASTIC_SEARCH_SERVER + "/golf/_search", args, function(data, response) {
-    var destinations = _.map(data.aggregations.results.buckets, function(bucket){
-      console.log(bucket);
-      return {
-        name : bucket.key,
-        rating : bucket.rating.value,
-        coords: bucket.geo.location
+  client.post(ELASTIC_SEARCH_SERVER + "/golf/_search")
+    .headers(config.defaultHeaders)
+    .send(config.destinationEsQuery)
+    .end(function(response) {
+      if (response.body) {
+        var destinations = _.map(response.body.aggregations.results.buckets, function(bucket) {
+          return {
+            name: bucket.key,
+            rating: bucket.rating.value,
+            coords: bucket.geo.location
+          }
+        });
+
+        // raw response
+        res.send(destinations);
+      } else {
+        console.log(response);
+        res.send([]);
       }
     });
+});
 
-    // raw response
-    res.send(destinations);
-  });
+app.use(function(err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
